92e345cb94dd967f293817d9393864961389a2ae
root <root@wolfsschanze.toorja>
2020-09-26 11:21:53 -0400

    MongoDB added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://graphql_test:kali1linux2@cluster0.kceji.mongodb.net/cluster0?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once('open', () => {
+    console.log('connected to MongoDB');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,9 @@ const { query } = require('express');
 const graphql = require('graphql');
 const _= require('lodash');
 
+const Book = require('../models/book');
+const Author = require('../models/author');
+
 const { 
     GraphQLObjectType, 
     GraphQLString, 
@@ -84,24 +87,24 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
         author: {
-            type: Author,
+            type: AuthorType,
             resolve(parent, args) {
-                return _.find(authors, {id: parent.authorId});
+                return Author.findById(parent.authorId);
             }
         }
     })
 });
 
-const Author = new GraphQLObjectType({
+const AuthorType = new GraphQLObjectType({
     name: 'Author',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         country: { type: GraphQLString },
-        book: {
+        books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return _.filter(books, {authorId: parent.id});
+                return Book.find({authorId: parent.id});
             }
         }
     })
@@ -116,34 +119,70 @@ const RootQuery = new GraphQLObjectType({
                 id:{ type: GraphQLID }
             },
             resolve(parent, args){
-                return _.find(books, {id: args.id});
+                return Book.findById(args.id);
             }
         },
         author:{
-            type: Author,
+            type: AuthorType,
             args:{
                 id:{ type: GraphQLID }
             },
             resolve(parent, args){
-                return _.find(authors, {id: args.id});
+                return Author.findById(args.id);
             }
         },
         book:{
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books;
+                return Book.find({});
             }
         },
         author:{
-            type: new GraphQLList(Author),
+            type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                return authors;
+                return Author.find({});
             }
         }
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                country: {type: GraphQLString}
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    country: args.country
+                });
+                return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLString}
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+    }
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
