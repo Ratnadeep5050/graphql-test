9b0af5898d5c217df5bb48df2a3993b2b04a8e30
root <root@wolfsschanze.toorja>
2020-09-27 13:25:48 -0400

    Added cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import BookList from './components/BookList';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-boost';
+
+
+const httpLink = new createHttpLink({
+  uri: 'https://127.0.0.1:4000/graphql'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div id="main">
+    <ApolloProvider client={client}>
+      <div id="main">
       <h1>Introvert Readers</h1>
       <BookList/>
     </div>
+    </ApolloProvider>  
   );
 }
 
