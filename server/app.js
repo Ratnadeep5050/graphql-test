5d4f788805354b58a169016e791955f5b102118f
root <root@wolfsschanze.toorja>
2020-09-27 13:27:08 -0400

    Added cors

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb+srv://graphql_test:kali1linux2@cluster0.kceji.mongodb.net/cluster0?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.once('open', () => {
